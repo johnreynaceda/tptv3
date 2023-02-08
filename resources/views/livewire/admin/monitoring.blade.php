f96722288c256021a4ce1f3900a52039c811c9ac
GabJeungSook <67619600+GabJeungSook@users.noreply.github.com>
2023-02-08 14:32:48 +0800

    user id change

--- resources/views/livewire/admin/monitoring.blade.php
+++ resources/views/livewire/admin/monitoring.blade.php
@@ -80,7 +80,7 @@
                 @forelse ($student_slot_details as $detail)
                   <tr>
                   <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
-                      {{ $detail->id }}
+                      {{ $detail->user_id }}
                     </td>
                     <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
                       {{ $detail->users->name }}
