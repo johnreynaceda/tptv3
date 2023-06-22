480f417618d99f6f303a63a9bb3602529a99f659
GabJeungSook <gicawalo@gmail.com>
2023-06-22 12:08:09 +0800

    added generate button

--- app/Http/Livewire/Admin/QualifiedStudentsReport.php
+++ app/Http/Livewire/Admin/QualifiedStudentsReport.php
@@ -12,41 +12,44 @@ class QualifiedStudentsReport extends Component
 {
     public $selected_campus;
     public $selected_program;
+    public $qualified_students;
+    public $campus_id;
+    public $program_id;
+
+
+    public function generateReport()
+    {
+        $this->qualified_students = Permit::whereHas('user.selected_courses', function ($query) {
+                $query->where('priority_level', 1);
+
+                if ($this->selected_campus) {
+                    $query->whereHas('program', function ($query) {
+                        $query->where('campus_id', $this->selected_campus);
+                    });
+                }
+
+                if ($this->selected_program) {
+                    $query->where('program_id', $this->selected_program);
+                }
+            })
+            ->join('results', 'permits.examinee_number', '=', 'results.examinee_number')
+            ->whereRaw('results.total_standard_score > 374')
+            ->get();
+    }
 
     public function render()
     {
-        $selected_campus_id = $this->selected_campus;
-        $selected_program_id = $this->selected_program;
-
-        $program_selects = Program::when($this->selected_campus, function ($query){
-            $query->where('campus_id', $this->selected_campus);
-        })
-        ->when($selected_campus_id, function ($query) use ($selected_campus_id) {
-            $query->where('campus_id', $selected_campus_id);
-        })
-        ->get();
-
-        $rankings = Permit::whereHas('user.selected_courses', function ($query) use ($selected_campus_id, $selected_program_id) {
-            $query->where('priority_level', 1);
-            if ($selected_campus_id) {
-                $query->whereHas('program', function ($query) use ($selected_campus_id) {
-                    $query->where('campus_id', $selected_campus_id);
-                });
-            }
-            if ($selected_program_id) {
-                $query->where('program_id', $selected_program_id);
-            }
-        })
-        ->join('results', 'permits.examinee_number', '=', 'results.examinee_number')
-        ->whereRaw('results.total_standard_score > 374')
-        ->paginate(100);
-
         $campuses = Campus::get();
 
+        $program_selects = Program::when($this->selected_campus, function ($query) {
+                $query->where('campus_id', $this->selected_campus);
+            })
+            ->get();
+
         return view('livewire.admin.qualified-students-report', [
-            'rankings' => $rankings,
+            'rankings' => $this->qualified_students,
             'campuses' => $campuses,
             'program_selects' => $program_selects
-            ]);
+        ]);
     }
 }
