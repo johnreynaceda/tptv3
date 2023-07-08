1085c48920202c3c332d9f921a4807c068473732
GabJeungSook <gicawalo@gmail.com>
2023-07-08 14:22:14 +0800

    test fromQuery method

--- app/Exports/QualifiedStudentsExport.php
+++ app/Exports/QualifiedStudentsExport.php
@@ -3,50 +3,53 @@
 namespace App\Exports;
 
 use App\Models\Permit;
-use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\WithMapping;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class QualifiedStudentsExport implements FromCollection, WithMapping, WithHeadings
+class QualifiedStudentsExport implements FromQuery, WithMapping, WithHeadings
 {
     public $examination_id;
-
-    /**
-    * @return \Illuminate\Support\Collection
-    */
 
     public function __construct($examination_id)
     {
         $this->examination_id = $examination_id;
     }
 
-    public function collection()
+    public function query()
     {
-        return Permit::whereHas('user.selected_courses', function ($query) {
-            $query->where('priority_level', 1);
-        })
-        ->join('results', 'permits.examinee_number', '=', 'results.examinee_number')
-        ->whereRaw('results.total_standard_score > 374')
-        ->get();
+        return Permit::query()
+            ->whereHas('user.selected_courses', function ($query) {
+                $query->where('priority_level', 1);
+            })
+            ->join('results', 'permits.examinee_number', '=', 'results.examinee_number')
+            ->whereRaw('results.total_standard_score > 374');
     }
 
-    public function map($permit) : array {
+    public function map($permit): array
+    {
         return [
             $permit->examinee_number,
-            $permit->user?->name,
-            $permit->result?->total_standard_score,
-            $permit->user?->selected_courses->where('priority_level', 1)->first()?->program->campus->name,
-            $permit->user?->selected_courses->where('priority_level', 1)->first()?->program->name,
-        ] ;
+            optional($permit->user)->name,
+            optional($permit->result)->total_standard_score,
+            optional($permit->user->selected_courses->where('priority_level', 1)->first())->program->campus->name,
+            optional($permit->user->selected_courses->where('priority_level', 1)->first())->program->name,
+        ];
     }
 
-    public function headings() : array {
+    public function headings(): array
+    {
         return [
             'Examinee Number',
             'Name',
             'Total Standard Score',
             'Campus',
             'Selected Course',
-        ] ;
+        ];
+    }
+
+    public function batchSize(): int
+    {
+        return 1000;
     }
 }
