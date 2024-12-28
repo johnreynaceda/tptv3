a67f7ce854a9a0e2817b137ab9d79f5df8cc4610
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-12-28 09:53:33 +0800

    uodate

--- resources/views/livewire/permit-layout.blade.php
+++ resources/views/livewire/permit-layout.blade.php
@@ -13,16 +13,16 @@
             font-size: 10pt;
         }
         .c-br{
-            border-right: 4px solid black;
+            border-right: 3px solid black;
         }
         .c-bl{
-            border-left: 4px solid black;
+            border-left: 3px solid black;
         }
         .c-bt{
-            border-top: 4px solid black;
+            border-top: 3px solid black;
         }
         .c-bb{
-            border-bottom: 4px solid black;
+            border-bottom: 3px solid black;
         }
 
         .custom-dashed {
@@ -43,7 +43,7 @@
                     <h1 class=" text-lg uppercase">Sultan Kudarat State University Tertiary Placement Test</h1>
                     <h2 class="font-bold uppercase text-xl">Test Permit</h2>
                 </div>
-                <div class="flex justify-end  min-w-28">
+                {{-- <div class="flex justify-end  min-w-28">
                     @if(!empty($permit))
                         <img src="data:image/png;base64,{{ DNS2D::getBarcodePNG($permit->generateQrData(), 'QRCODE') }}" 
                              alt="QR Code" 
@@ -54,7 +54,7 @@
                             <p>Schedule</p>
                         </div>
                     @endif
-                </div>
+                </div> --}}
                
             </div>
     
@@ -112,32 +112,43 @@
                
             </div>
     
-            <div class="mt-4 grid grid-cols-2 gap-4 ">
-                <div class="">
+            <div class="mt-4 grid grid-cols-2 gap-4 c-bt c-bb ">
+                <div class="c-br p-2">
                     <h3 class="text-sm font-semibold  border-black">Appointment Details</h3>
                     <p class="text-sm"><span class="font-bold">Date of Exam:</span> February 2, 2024</p>
                     <p class="text-sm"><span class="font-bold">Time:</span> 07:30AM</p>
                     <p class="text-sm"><span class="font-bold">Testing Site:</span> ACCESS Campus</p>
                 </div>
-                <div>
+                <div class="p-2">
                     <h3 class="text-sm font-semibold  border-black">Examination Requirements</h3>
-                    <ul class="list-disc list-inside text-sm">
-                        <li>Printed Test Permit (A4 or Short Size Bond Paper)</li>
-                        <li>Pencil No. 2 with Eraser</li>
-                        <li>School ID or any Valid ID</li>
-                    </ul>
+                    <ol class="list-decimal list-inside text-sm" >
+                        <li class="">Printed Test Permit <span class="text-xs text-gray-600 italic">(A4 or Short Size Bond Paper)</span></li>
+                        <li class="">Pencil No. 2 with Eraser</li>
+                        <li class="">School ID or any Valid ID</li>
+                    </ol>
                 </div>
             </div>
     
             <div class="mt-4">
-                <h3 class="text-sm font-semibold ">Important Reminders</h3>
-                <ol class="list-decimal list-inside text-sm space-y-1">
-                    <li>All examinees are required to wear closed shoes, white polo shirt or T-shirt, and long pants or long skirt. Strictly no tattered pants and long slit skirt.</li>
-                    <li>Admission of examinees is on a First Come, First Serve Basis. All examinees are advised to proceed at the testing site 30 minutes before the examination starts. Late examinees will no longer be entertained.</li>
-                    <li>Parents or guardians are not allowed to enter the university premises during the examination.</li>
-                    <li>You are only allowed to take the examination once. Taking the examination twice will result in disqualification of your application for admission to SKSU.</li>
-                    <li>Backpack or any other large bags are not allowed inside the university premises.</li>
+                <h2 class="font-bold uppercase text-xl text-center">Important Reminders</h2>
+                <ol class="list-decimal list-inside text-sm  pl-4">
+                    <li class="">
+                        All examinees are required to wear closed shoes, white polo shirt or T-shirt, and long pants or long skirt. Strictly no tattered pants and long slit skirt.
+                    </li>
+                    <li class="">
+                        Admission of examinees is on a First Come, First Serve Basis. All examinees are advised to proceed at the testing site 30 minutes before the examination starts. Late examinees will no longer be entertained.
+                    </li>
+                    <li class="">
+                        Parents or guardians are not allowed to enter the university premises during the examination.
+                    </li>
+                    <li class="">
+                        You are only allowed to take the examination once. Taking the examination twice will result in disqualification of your application for admission to SKSU.
+                    </li>
+                    <li class="">
+                        Backpack or any other large bags are not allowed inside the university premises.
+                    </li>
                 </ol>
+                
             </div>
             
             <div class="my-4">
