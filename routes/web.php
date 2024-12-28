07915171d8e267114e98d93a1a1c1e6ce1276abd
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-12-28 14:09:08 +0800

    dasd

--- routes/web.php
+++ routes/web.php
@@ -142,8 +142,8 @@ Route::prefix('/admin')
       
             
                 $pdfContent = Browsershot::url('https://spatie.be/docs/browsershot/v4/usage/creating-pdfs') 
-                ->setNodeBinary('/root/.nvm/versions/node/v22.12.0/bin/node')
-                ->setNpmBinary('/root/.nvm/versions/node/v22.12.0/bin/npm')
+                ->setNodeBinary('/usr/local/bin/node')
+                ->setNpmBinary('/usr/local/bin/npm')
                 ->setOption('args', ['--no-sandbox']) // Required for server environments
                 ->pdf(); 
             
