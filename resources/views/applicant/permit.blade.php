29d3f65ffb18002092b5e0a5c7b0103fb6f8aef9
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-12-29 02:04:09 +0800

    updatd

--- resources/views/applicant/permit.blade.php
+++ resources/views/applicant/permit.blade.php
@@ -58,7 +58,7 @@
       <!-- Download PDF Button -->
       <a href="{{ route('admin.generate-pdf-permit', $permit) }}"
          class="flex items-center px-4 py-2 text-white bg-green-500 hover:bg-green-600 font-medium text-sm rounded-md shadow focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
-          <x-heroicon-o-document-download class="w-5 h-5 mr-2"/> <!-- Optional: Icon -->
+         
           Download PDF
       </a>
   </div>
