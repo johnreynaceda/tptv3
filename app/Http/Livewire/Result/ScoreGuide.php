1fd70be0b14dd64de789d03871e17190ba06d317
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2025-06-06 09:48:04 +0800

    3123

--- app/Http/Livewire/Result/ScoreGuide.php
+++ app/Http/Livewire/Result/ScoreGuide.php
@@ -7,19 +7,16 @@ use App\Models\{Permit,Result};
 
 class ScoreGuide extends Component
 {
-    public $examinee_number;
+    
     public function mount()
     {
-        if(auth()->user()->permit != null)
-        {
-            $this->examinee_number = auth()->user()->permit->examinee_number_updated;
-        }
+
 
     }
     public function render()
     {
         return view('livewire.result.score-guide',[
-            'result'=>Result::where('examinee_number',$this->examinee_number)->first()
+
         ]);
     }
 }
