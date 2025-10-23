4dec883b2df508d9e1493de1ec03c9a6cf8fc05d
Tristan Sarceda <orbinobrian0506@gmail.com>
2025-10-23 15:38:19 +0800

    updates

--- resources/views/livewire/applicant/personal-info.blade.php
+++ resources/views/livewire/applicant/personal-info.blade.php
@@ -70,7 +70,7 @@
                         @else
 
                                 Save
-               
+
                         @endif
                     @endif
 button below to confirm.
@@ -149,18 +149,7 @@ button below to confirm.
                 ✅ Photo captured! Preview above.
             </div>
 
-            {{-- Upload loading indicator --}}
-            <div x-show="uploading" class="mt-2 text-gray-600">
-                <svg class="animate-spin h-5 w-5 inline-block" xmlns="http://www.w3.org/2000/svg" fill="none"
-                    viewBox="0 0 24 24">
-                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
-                        stroke-width="4"></circle>
-                    <path class="opacity-75" fill="currentColor"
-                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
-                    </path>
-                </svg>
-                Uploading photo...
-            </div>
+       
 
             {{-- Buttons --}}
             <div class="flex justify-center gap-2 mt-4">
