e7fb1a7f5b9538e0d2edaf49242b8302ec80fdeb
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2026-02-26 21:46:09 +0800

    update

--- resources/views/result-pdf-2026.blade.php
+++ resources/views/result-pdf-2026.blade.php
@@ -391,7 +391,7 @@
                         <td class="score">400</td>
                     </tr>
                     <tr class="row-odd">
-                        <td>Bachelor of Science in Criminal Justice Education</td>
+                        <td>BS in Criminology</td>
                         <td class="score">450</td>
                         <td colspan="2" class="campus-header">KALAMANSIG CAMPUS</td>
                     </tr>
@@ -439,7 +439,7 @@
                     <tr class="row-odd">
                         <td>Bachelor of Science in Information System</td>
                         <td class="score">400</td>
-                        <td>Bachelor of Science in Criminal Justice Education</td>
+                        <td>BS in Criminology</td>
                         <td class="score">425</td>
                     </tr>
                     <tr class="row-even">
